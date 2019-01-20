13289277588a2c11a5bb631dfb6e6dcc8ec83acb
Adrian Jablonski <Jablonski.Adrian1@gmail.com>
2019-01-20 10:58:53 -0600

    Added overloading to execute method

--- CalcEngine/src/com/example/calcengine/Main.java
+++ CalcEngine/src/com/example/calcengine/Main.java
@@ -16,5 +16,29 @@ public class Main {
             System.out.println(equation.getResult());
         }
 
+        System.out.println();
+        System.out.println("Using Overloads");
+        System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt = 9;
+        int rightInt = 4;
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResult());
+
+        equationOverload.execute(leftInt, rightInt);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResult());
+
+        // The below does an automatic widening conversion on the int parameter to a double
+        equationOverload.execute(leftInt, rightDouble);
+        System.out.print("result=");
+        System.out.println(equationOverload.getResult());
+
     }
 }
