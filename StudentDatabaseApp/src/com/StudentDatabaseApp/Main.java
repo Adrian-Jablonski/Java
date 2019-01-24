370da2a0e27feebeeedc05b0724acf05534d6560
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-01-23 22:05:39 -0600

    Added multiple student creation functionality

--- StudentDatabaseApp/src/com/StudentDatabaseApp/Main.java
+++ StudentDatabaseApp/src/com/StudentDatabaseApp/Main.java
@@ -1,15 +1,27 @@
 package com.StudentDatabaseApp;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        stu1.showInfo();
-	    // Ask how many new users we want to add
+	    // Ask how many new students we want to add
+        System.out.print("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
 
         // Create n number of new students
+        for (int i = 0; i < numOfStudents; i++) {
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+        }
+
+        for (int i = 0; i < numOfStudents; i++) {
+            students[i].showInfo();
+            System.out.println("--------------");
+        }
 
 
     }
